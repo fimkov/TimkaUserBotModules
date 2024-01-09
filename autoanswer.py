7811cb70d3a72f306d1fee1f02f618511dfed3f5
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-09 18:11:47 +0700

    Update autoanswer.py

--- autoanswer.py
+++ autoanswer.py
@@ -2,32 +2,68 @@ from pyrogram import Client, filters
 from helps.modules import add_module, add_command
 from helps.scripts import get_keys, set_key
 from plugins.autoreactions import auto_reaction
-
 from helps.get_prefix import get_prefix
+from helps.scripts import get_lang
+
+lang = get_lang()
 prefix = get_prefix()
+
+
+class Texts:
+    @staticmethod
+    def get_texts(info):
+        return {
+            "aws_info": {
+                "ru": f"На данный момент у aws такие настройки:\n\nСтатус {info[1]}\nТекст: {info[0]}",
+                "en": f"At the moment, aws has these settings:\n\n Status {info[1]}\Text: {info[0]}"
+            },
+            "aws_status": {
+                "on": {
+                    "ru": "autoanswer включен",
+                    "en": "autoanswer on"
+                },
+                "off": {
+                    "ru": "autoanswer выключен",
+                    "en": "autoanswer off"
+                }
+            },
+            "error": {
+                "ru": "После аргумента set должен быть текст!",
+                "en": "There should be text after the set argument!"
+            },
+            "successfully": {
+                "ru": "Текст успешно установлен!",
+                "en": "Text successfully installed!"
+            }
+        }
+
 
 @Client.on_message(filters.command("aws", prefixes=prefix) & filters.me)
 async def aws(client, message):
+    info = await get_keys(['aws_text', 'aws_status'])
+    text_versions = Texts.get_texts(info)
+
     try:
         command = message.command[1]
     except IndexError:
-        info = await get_keys(['aws_text', 'aws_status'])
-        await message.edit(f"На данный момент у aws такие настройки:\n\nСтатус {info[1]}\nТекст: {info[0]}")
+        await message.edit(text_versions['aws_info'][lang])
         return
+
     if command == 'on':
         await set_key('aws_status', 'on')
-        await message.edit('autoanswer включен')
+        await message.edit(text_versions['aws_status']['on'][lang])
     elif command == 'off':
         await set_key('aws_status', 'off')
-        await message.edit('autoanswer выключен')
+        await message.edit(text_versions['aws_status']['off'][lang])
     elif command == 'set':
         try:
             text = " ".join(message.command[2:])
         except IndexError:
-            await message.edit('После аргумента set должен быть текст!')
+            await message.edit(text_versions['error'][lang])
             return
         await set_key('aws_text', text)
-        await message.edit("Текст успешно установлен!")
+        await message.edit(text_versions['successfully'][lang])
+
 
 @Client.on_message(filters.private & ~filters.me & ~filters.bot)
 async def autoasw(client, message):
@@ -44,8 +80,13 @@ async def autoasw(client, message):
         pass
     await auto_reaction(client, message)
 
-
-add_module('autoanswer', __file__)
-add_command('autoanswer', f'{prefix}aws on', 'включить автоответчик')
-add_command('autoanswer', f'{prefix}aws off', 'выключить автоответчик')
-add_command('autoanswer', f'{prefix}aws set [текст]', 'установить текст для отправки')
+if lang == "ru":
+    add_module('autoanswer', __file__)
+    add_command('autoanswer', f'{prefix}aws on', 'включить автоответчик')
+    add_command('autoanswer', f'{prefix}aws off', 'выключить автоответчик')
+    add_command('autoanswer', f'{prefix}aws set [текст]', 'установить текст для отправки')
+else:
+    add_module('autoanswer', __file__)
+    add_command('autoanswer', f'{prefix}aws on', 'turn on the answering machine')
+    add_command('autoanswer', f'{prefix}aws off', 'turn off the answering machine')
+    add_command('autoanswer', f'{prefix}aws set [text]', 'set the text to send')
