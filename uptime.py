069eedcff9ff112706d6f155555cc4afe29ce004
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-25 17:22:19 +0700

    Update uptime.py

--- uptime.py
+++ uptime.py
@@ -1,8 +1,10 @@
 import datetime
 from pyrogram import Client, filters
 from helps.modules import add_module, add_command
-
 from helps.get_prefix import get_prefix
+from helps.scripts import get_lang
+
+lang = get_lang()
 prefix = get_prefix()
 
 @Client.on_message(filters.command("uptime", prefixes=prefix))
@@ -12,5 +14,10 @@ async def uptime_get(client, message):
     )
 
 start = datetime.datetime.now().replace(microsecond=0)
-add_module("uptime", __file__)
-add_command("uptime", f"{prefix}uptime", "показывает аптайм юзербота")
+
+if lang == "ru":
+    add_module("uptime", __file__)
+    add_command("uptime", f"{prefix}uptime", "показывает аптайм юзербота")
+else:
+    add_module("uptime", __file__)
+    add_command("uptime", f"{prefix}uptime", "shows userbot uptime")
